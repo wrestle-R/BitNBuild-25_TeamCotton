ea83210133b8c2bb3ba13450b28296fe910580c1
Romeiro <theromeirofernandes@gmail.com>
2025-09-27 19:44:36 +0530

    removed things not being used

--- frontend/src/components/Landing/LandingPage.jsx
+++ frontend/src/components/Landing/LandingPage.jsx
@@ -3,11 +3,9 @@ import { useUserContext } from '../../../context/UserContextSimplified';
 import Navbar from '../ui/Navbar';
 import HeroSection from './HeroSection';
 import AboutSection from './AboutSection';
-import ArenaSection from './ArenaSection';
 import TestimonialsSection from './TestimonialsSection';
 import ContactSection from './ContactSection';
 import FooterSection from './FooterSection';
-import { LogoCloud } from '../ui/LogoCloud';
 
 const LandingPage = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -23,11 +21,9 @@ const LandingPage = () => {
       
       <main className="overflow-hidden">
         <HeroSection user={user} />
-        {/* <LogoCloud /> */}
       </main>
 
       <AboutSection />
-      {/* <ArenaSection /> */}
       <TestimonialsSection />
       <ContactSection user={user} />
       <FooterSection />
--- frontend/src/components/Landing/index.js
+++
@@ -1,7 +0,0 @@
-export { default as LandingPage } from './LandingPage';
-export { default as HeroSection } from './HeroSection';
-export { default as AboutSection } from './AboutSection';
-export { default as ArenaSection } from './ArenaSection';
-export { default as TestimonialsSection } from './TestimonialsSection';
-export { default as ContactSection } from './ContactSection';
-export { default as FooterSection } from './FooterSection';
