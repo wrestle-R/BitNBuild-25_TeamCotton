b1f77941a23ce54213f3f4bb62d7878c0e1f665f
Adityadab10 <adityadab27@gmail.com>
2025-09-27 17:46:24 +0530

    driverapp login page done

--- driverApp/app/(tabs)/index.jsx
+++ driverApp/app/(tabs)/index.jsx
@@ -7,13 +7,12 @@ import ParallaxScrollView from '../../components/ParallaxScrollView';
 import { ThemedText } from '../../components/ThemedText';
 import { ThemedView } from '../../components/ThemedView';
 import '../../global.css';
+import Login from '../../components/Login';
 
 export default function HomeScreen() {
   return (
    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-blue-500 text-2xl font-bold">
-        NativeWind is working! 🎉
-      </Text>
+      <Login />
     </View>
   );
 }
